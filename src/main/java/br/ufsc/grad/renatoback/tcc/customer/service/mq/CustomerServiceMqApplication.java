7bbc6d7f7f6bc9f99d344e607adeac3608a24d52
renato <renato@knewin.com>
2016-11-20 03:22:50 -0200

    Trocando servidor AMQP.

--- src/main/java/br/ufsc/grad/renatoback/tcc/customer/service/mq/CustomerServiceMqApplication.java
+++ src/main/java/br/ufsc/grad/renatoback/tcc/customer/service/mq/CustomerServiceMqApplication.java
@@ -31,7 +31,7 @@ public class CustomerServiceMqApplication {
 		final URI ampqUrl;
 		try {
 			// ampqUrl = new URI(getEnvOrThrow("CLOUDAMQP_URL"));
-			ampqUrl = new URI("amqp://mznziqrv:f6Z3hHj7WGIqbHmxnFTUURiS2XzUhtMH@buck.rmq.cloudamqp.com/mznziqrv");
+			ampqUrl = new URI("amqp://sdfhkyxm:aVRKdSrW4aoklXzTDd_N4HmLHTzKYSUe@buck.rmq.cloudamqp.com/sdfhkyxm");
 		} catch (URISyntaxException e) {
 			throw new RuntimeException(e);
 		}
